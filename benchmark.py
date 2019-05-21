356c730d2304d828e2ced0ba40a404f6f7e95deb
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-05-21 12:28:44 -0300

    Finish linux instalation

--- benchmark.py
+++ benchmark.py
@@ -1,34 +1,15 @@
 import os
-from subprocess import check_output
+from utils import *
 
 dirName = 'results'
 results_file_extension = '.txt'
 
-
-def install_application(application_name):
-    try:
-        check_output([application_name, '--version'])
-    except:
-        print(application_name + ' not installed')
-        print('installing ' + application_name + '...')
-        os.system('apt-get install ' + application_name)
-
-
+install_geekbench()
 install_application('sysbench')
 install_application('stress-ng')
 
-
 if not os.path.exists(dirName):
     os.mkdir(dirName)
-
-def benchmark_call(command, benchmark_name):
-    print('Starting ' + benchmark_name)
-    res = os.system(command + " > " + os.path.join(dirName, benchmark_name + results_file_extension))
-    if res is not None:
-        print(benchmark_name + 'ended with code: ' + res)
-    else:
-        print(benchmark_name + 'ended')
-
 
 benchmark_call("./geekbench4", 'geekbench')
 benchmark_call("sysbench --test=cpu --cpu-max-prime=50000 run", 'sysbench')
