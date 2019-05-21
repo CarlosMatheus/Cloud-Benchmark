d2cdbc20f344a29d1853ff6f927b97435176355d
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-05-21 10:49:11 -0300

    Adjust benchmark function

--- benchmark.py
+++ benchmark.py
@@ -3,13 +3,18 @@ import os
 dirName = 'results'
 results_file_extension = '.txt'
 
+
 if not os.path.exists(dirName):
     os.mkdir(dirName)
 
 
 def benchmark_call(command, benchmark_name):
-    print('Starting ' + benchmark_name + )
-    os.system(command + " > " + os.path.join(dirName, benchmark_name + results_file_extension))
+    print('Starting ' + benchmark_name)
+    res = os.system(command + " > " + os.path.join(dirName, benchmark_name + results_file_extension))
+    if res is not None:
+        print(benchmark_name + 'ended with code: ' + res)
+    else:
+        print(benchmark_name + 'ended')
 
 
 benchmark_call("./geekbench4", 'geekbench')
