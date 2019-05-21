83f84c63a859291720b882a7b8e9748ef66f6e60
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-05-21 11:24:50 -0300

    Install applications

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,25 @@
 import os
+from subprocess import check_output
 
 dirName = 'results'
 results_file_extension = '.txt'
 
 
+def install_application(application_name):
+    try:
+        check_output([application_name, '--version'])
+    except:
+        print(application_name + ' not installed')
+        print('installing ' + application_name + '...')
+        os.system('apt-get install ' + application_name)
+
+
+install_application('sysbench')
+install_application('stress-ng')
+
+
 if not os.path.exists(dirName):
     os.mkdir(dirName)
-
 
 def benchmark_call(command, benchmark_name):
     print('Starting ' + benchmark_name)
