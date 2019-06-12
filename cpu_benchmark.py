b2dbe6b7bb883c2b3baa76f209f7f286f4c80644
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-12 03:50:14 -0300

    Add separators

--- cpu_benchmark.py
+++ cpu_benchmark.py
@@ -7,7 +7,14 @@ install_application('stress-ng')
 if not os.path.exists(dirName):
     os.mkdir(dirName)
 
+
+def print_sep():
+    print('================================================================')
+
+
+print_sep()
 print('Note: 1 bogo ops is one iteration of what the test is doing')
+print_sep()
 print('Testing CPU with specific methods')
 benchmark_call("stress-ng --cpu 1 --cpu-method sdbm --metrics-brief --perf -t 5", 'stress')
 benchmark_call("stress-ng --cpu 1 --cpu-method int128decimal64 --metrics-brief --perf -t 5", 'stress')
@@ -15,10 +22,13 @@ benchmark_call("stress-ng --cpu 1 --cpu-method parity --metrics-brief --perf -t 
 benchmark_call("stress-ng --cpu 1 --cpu-method rand --metrics-brief --perf -t 5", 'stress')
 benchmark_call("stress-ng --cpu 1 --cpu-method hamming --metrics-brief --perf -t 5", 'stress')
 benchmark_call("stress-ng --cpu 1 --cpu-method matrixprod --metrics-brief --perf -t 5", 'stress')
+print_sep()
 print('Testing CPU using all metrics, each iteration is one metric and they are done in order')
 benchmark_call("stress-ng --cpu 1 --cpu-method all --metrics-brief --perf -t 10", 'stress')
+print_sep()
 print('Testing the hd, each iteration is executed writes and reads')
 benchmark_call("stress-ng --hdd 1  --metrics-brief --perf -t 10", 'stress')
+print_sep()
 print('Testing the memory, in which each iteration malloc(3), calloc(3), realloc(3) and  free(3).')
 benchmark_call("stress-ng --vm 1 --vm-bytes 50% --vm-method all --verify -t 15 -v --metrics-brief --p", 'stress')
 benchmark_call("sysbench --test=cpu --cpu-max-prime=100000 run", 'sysbench')
