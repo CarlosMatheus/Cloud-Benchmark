07cc4487346ec11d9bc8ab564f27df4c1ab40d18
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-21 01:07:01 -0300

    Treat server download and upload results

--- treat_results.py
+++ treat_results.py
@@ -17,7 +17,6 @@ def create_folder(test_name):
 
 
 def write_results(test_name, benchmark_name, tests_names, results):
-    print(results)
     create_folder(test_name)
     output_file = open('treated_results/' + test_name + '/' + benchmark_name + '.csv', 'w')
     output_file.write(','.join(tests_names) + '\n')
@@ -101,10 +100,44 @@ def treat_client_ping_result(test_name):
     write_results(test_name, input_file_name, ['ping_mean'], results)
 
 
+def treat_client_iperf3_result(test_name):
+    input_file_name = 'client_iperf3'
+
+    file = open('results/' + test_name + '/' + input_file_name + '.txt', 'r')
+    text = file.read()
+
+    iteration_texts = text.split('Executing client_iperf3')
+    del iteration_texts[0]
+
+    iteration_texts_by_line = [n.split('\n') for n in iteration_texts]
+
+    for idx, iteration in enumerate(iteration_texts_by_line):
+
+        values = []
+        for line in iteration:
+            if ('sender' in line or 'receiver' in line) and 'Mbits' in line:
+                values.append(line)
+
+        values[0] = values[0].split(' Mbits')[0]
+        values[0] = values[0].split('MBytes')[-1]
+        values[0] = values[0].split(' ')[-1]
+
+        values[1] = values[1].split(' Mbits')[0]
+        values[1] = values[1].split('MBytes')[-1]
+        values[1] = values[1].split(' ')[-1]
+
+        iteration_texts_by_line[idx] = values
+
+    results = iteration_texts_by_line
+    print(results)
+
+    write_results(test_name, input_file_name, ['server_download', 'server_upload'], results)
+
+
 tests = get_tests_names()
 
 for test in tests:
     treat_stress_result(test)
     treat_sysbench_result(test)
     treat_client_ping_result(test)
-
+    treat_client_iperf3_result(test)
