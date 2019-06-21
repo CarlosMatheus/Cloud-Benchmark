968ce10cddabdd503a1a266d9534450f7914f6b4
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-20 23:51:47 -0300

    Treat sysbench result

--- treat_res.py
+++ treat_res.py
@@ -11,6 +11,15 @@ def create_folder(test_name):
         os.mkdir(os.path.join(dirName, test_name))
 
 
+def write_results(test_name, benchmark_name, tests_names, results):
+    create_folder(test_name)
+    output_file = open('treated_results/' + test_name + '/' + benchmark_name + '.csv', 'w')
+    output_file.write(','.join(tests_names) + '\n')
+
+    for result in results:
+        output_file.write(','.join(result) + '\n')
+
+
 def treat_stress_result(test_name):
 
     def get_results_per_iter_per_test(iteration_texts_by_test_by_line):
@@ -43,18 +52,29 @@ def treat_stress_result(test_name):
     results_per_iter_per_test = get_results_per_iter_per_test(iteration_texts_by_test_by_line)
     results = get_results(results_per_iter_per_test)
 
-    create_folder(test_name)
-    output_file = open('treated_results/' + test_name + '/stress.csv', 'w')
-
     tests_names = ['sdbm', 'int128decimal64', 'parity', 'rand',
                    'hamming', 'matrixprod', 'all_metrics', 'hd', 'memory']
 
-    output_file.write(','.join(tests_names) + '\n')
+    write_results(test_name, 'stress', tests_names, results)
 
-    for result in results:
-        output_file.write(','.join(result) + '\n')
+
+def treat_sysbench_result(test_name):
+    file = open('results/' + test_name + '/sysbench.txt', 'r')
+    text = file.read()
+    iteration_texts = text.split('Executing sysbench')
+    del iteration_texts[0]
+    iteration_texts_by_line = [n.split('\n') for n in iteration_texts]
+
+    for idx, iteration in enumerate(iteration_texts_by_line):
+        iteration_texts_by_line[idx] = iteration[18].split(':')[-1]
+        iteration_texts_by_line[idx] = list(filter(lambda a: a != '', iteration_texts_by_line[idx].split(' ')))
+
+    results = iteration_texts_by_line
+
+    write_results(test_name, 'sysbench', ['sysbench_primes'], results)
 
 
 for test in tests:
     treat_stress_result(test)
+    treat_sysbench_result(test)
 
